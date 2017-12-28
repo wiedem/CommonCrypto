df983fe640d82bdfbebe2d9d8795c9ede942d2de
Holger Wiedemann <holger.wiedemann@diva-e.com>
2017-12-28 01:23:40 +0100

    Added framework version header file exposing the automatically generated version functions. Fixes archive builds not creating the public header folder of the framework.

---
+++ CommonCrypto/CommonCryptoWrapper.h
@@ -0,0 +1,12 @@
+//
+//  CommonCryptoWrapper.h
+//  CommonCrypto
+//
+
+@import Foundation;
+
+//! Project version number for CommonCrypto.
+FOUNDATION_EXPORT double CommonCryptoVersionNumber;
+
+//! Project version string for CommonCrypto.
+FOUNDATION_EXPORT const unsigned char CommonCryptoVersionString[];
